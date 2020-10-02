d5406b2794e5eabe106acf6e3353ccebb033b1b8
Daniel <dapascale@gmail.com>
2020-10-02 17:36:31 -0400

    returning names AND symbols

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import Axios from 'axios';
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
@@ -15,43 +14,56 @@ class App extends Component {
     }
   }
 
+
   componentDidMount() {
+    // Create an axios call to retrieve data from each country
     axios({
       url: 'https://restcountries.eu/rest/v2/all',
     }).then(res => {
+      // With results, create a variable to store data
       const countries = res.data;
 
+      // Create an empty array
       const newCountries = [];
+      // Loop through each country's data
       countries.forEach(country => {
+        // For each country, store the country name in a variable
         const name = country.name;
-        newCountries.push(name);
-
-        this.setState({
-          countryNames: newCountries,
-        })
 
         const countryCurrencies = country.currencies;
         countryCurrencies.forEach(currency => {
-          let code = currency.code;
+          let symbol = currency.code;
 
+          newCountries.push({name: name, symbol: symbol});
+
+          this.setState({
+            countryNames: newCountries,
+          })
           // THIS IS WHAT WE USE TO TARGET THE OTHER API FOR THE $$ AMT
-          console.log(code, name);
+          // console.log(code, name);
           // if code == code from currencies.js, return that $$ from currencies state
         })
+        // console.log(newCountries)
       })
     })
   }
 
-  
+  handleChange = (event) => {
+    const userSelection = event.target.value;
+    console.log(userSelection)
+    // this.setState({
+    //   currentSymbol: userSelection,
+    // })
+  }
 
   render() {
     return (
       <div className="App">
-        <select>
-          {this.state.countryNames.map(name => {
+        <select onChange={this.handleChange}>
+          {this.state.countryNames.map((country, index) => {
             return (
-              <option key={name} value={name}>
-                {name}
+              <option key={index}>
+                {`${country.name} - ${country.symbol}`}
               </option>
             )
             })
